6d8408400630770a20c98c5369c070c88a327488
Dan <daniil.coftyn@qwire.com>
2019-11-20 01:57:38 +0200

    ForTestingStuff

---
+++ index.js
@@ -0,0 +1,42 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '1057147927:AAGwzeIQ2ptYKYKEH2GnUPSIQ3fx-TciOtA';
+var bot = new TelegramBot(token, {polling: true});
+
+var notes = [];
+
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+
+setInterval(function(){
+    for (var i = 0; i < notes.length; i++) {
+    const curDate = new Date().getHours() + ':' + new Date().getMinutes();
+    if (notes[i]['time'] === curDate) {
+      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+      notes.splice(i, 1);
+    }
+  }
+}, 1000);
+
+});
+
+bot.onText(/\/love/, function onLoveText(msg) {
+  const opts = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: [
+        ['Yes, you are the bot of my life ❤'],
+        ['No, sorry there is another one...']
+      ]
+    })
+  };
+  bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
+});
