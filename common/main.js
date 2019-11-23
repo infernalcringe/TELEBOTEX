d3280c517eb163d4c89ae16fcd18bf6ef1c30f38
infernalcringe <44805188+infernalcringe@users.noreply.github.com>
2019-11-23 15:34:41 +0200

    Update main.js

--- common/main.js
+++ common/main.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const getWinner = require('./small_features');
-const token = '1057147927:AAGwzeIQ2ptYKYKEH2GnUPSIQ3fx-TciOtA';
+const token = '';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on("polling_error", (err) => console.log(err));
